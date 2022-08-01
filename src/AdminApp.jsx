a9bd368776e8468822740259e22317ef2e826045
Magin Luna <magin1104@gmail.com>
2022-08-01 12:56:04 -0400

    LoginPage with navigation

--- src/AdminApp.jsx
+++ src/AdminApp.jsx
@@ -1,44 +1,9 @@
 
-import { LoginPage } from "./pages/LoginPage";
+import { AppRoute } from "./router/AppRoute";
 export const AdminApp = () => {
   return (
     <>
-      <LoginPage/>
-      {/* <div className="d-flex">
-        <div className="background-medium"></div>
-        <div className="container-login">
-          <div className="col-12">
-            <div className="logo">
-              <svg>
-                <use xlinkHref={`${icons}#svg-users`} />
-              </svg>
-            </div>
-            <Typography align="center" variant="h6" component="h2">
-              Hello Again!
-            </Typography>
-            <p className="parrafo mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-              vitae nulla ratione doloribus sunt quod!
-            </p>
-            <TextField
-              id="outlined-basic"
-              label="name@example.com"
-              variant="outlined"
-              sx={{ mb: 2, width: "100%" }}
-            />
-            <TextField
-              type="password"
-              id="outlined-basic"
-              label="Password"
-              variant="outlined"
-              sx={{ mb: 2, width: "100%" }}
-            />
-            <Button sx={{ mb: 2, width: "100%" }} variant="outlined">
-              Ingresar
-            </Button>
-          </div>
-        </div>
-      </div> */}
+      <AppRoute/>
     </>
   );
 };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,44 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 import icons from "../assets/svg/svg-icons.svg";
 import "../assets/styles/index.css";
 import "@fontsource/roboto/300.css";
+import { useEffect, useState } from "react";
 export const LoginPage = () => {
+  const [formState, setformState] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const { email, password } = formState;
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setformState({
+      ...formState,
+      [name]: value,
+    });
+  };
+  useEffect(() => {}, [formState]);
+  const regex = new RegExp(/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/);
+  const onLogin = () => {
+    navigate("/admin", {
+      replace: true,
+    });
+  };
   return (
-    <div className="body_login">
-      <div className="lateral-card">hola mundo</div>
-      <div className="login_area">
+    <div className="left_area">
+      <div className="left_area_contain">
+          <svg className="left-icon">
+            <use xlinkHref={`${icons}#svg-park`} />
+          </svg>
+      </div>
+      <div className="right_area">
         <div className="card-login">
           <div className="logo">
             <svg>
-              <use xlinkHref={`${icons}#svg-users`} />
+              <use xlinkHref={`${icons}#svg-avatar`} />
             </svg>
           </div>
           <Typography align="center" variant="h6" component="h2">
@@ -23,17 +49,43 @@ export const LoginPage = () => {
             vitae nulla ratione doloribus sunt quod!
           </p>
           <TextField
-            id="outlined-basic"
+            name="email"
+            id="email"
             label="email@example.com"
-            variant="outlined"
+            onChange={onInputChange}
             sx={{ mb: 2, width: "100%" }}
+            value={email}
+            variant="standard"
+            error={
+              email.length > 0 ? (regex.test(email) ? false : true) : false
+            }
+            helperText={
+              email.length > 0 ? (regex.test(email) ? "" : "Invalid email") : ""
+            }
           />
           <TextField
+            name="password"
+            id="password"
             type="password"
-            id="outlined-basic"
             label="Password"
-            variant="outlined"
+            onChange={onInputChange}
             sx={{ mb: 2, width: "100%" }}
+            value={password}
+            variant="standard"
+            error={
+              password.length > 0
+                ? password.length >= 6
+                  ? false
+                  : true
+                : false
+            }
+            helperText={
+              password.length > 0
+                ? password.length >= 6
+                  ? ""
+                  : "Enter more 5 characters"
+                : ""
+            }
           />
           <Button
             sx={{
@@ -44,10 +96,17 @@ export const LoginPage = () => {
               borderBlockColor: "#3f50b5",
             }}
             variant="outlined"
+            disabled={
+              email.length > 0 && password.length >= 6
+                ? regex.test(email)
+                  ? false
+                  : true
+                : true
+            }
+            onClick={onLogin}
           >
             Sign In
           </Button>
-          <a href="/">Sign Up</a>
         </div>
       </div>
     </div>
